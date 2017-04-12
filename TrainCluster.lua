13d5a8e55c808b3902024ffc5fc9911661e3eddf
Elod Csirmaz <csirmazelodpal@gmail.com>
2017-04-12 02:05:58 +0100

    Training on GPU & debug output

--- TrainCluster.lua
+++ TrainCluster.lua
@@ -1,14 +1,20 @@
 require 'torch'
-local nn = require 'nn'
+require 'nn'
+
+require 'cutorch'
+require 'cunn'
+
+-- cutorch.setDevice(1)
+dtype = 'torch.CudaTensor'
+
 
 local inputSize = 5;
 local dimensions = 2;
 local clusters = 4;
+local hiddenSize = 12;
+local depth = 2;
 
 local function createModel()
-
-   local hiddenSize = 12;
-   local depth = 2;
    
    local s = nn.Sequential()
    
@@ -18,17 +24,22 @@ local function createModel()
    -- inputSize == ?
    -- outputSize == dimensions
    
-   -- http://www.epcsirmaz.com/torch/torch_nn-simple_layers-linear.html
-   s:add(nn.Linear(inputSize, hiddenSize))
+   if depth == 0 then
+      -- http://www.epcsirmaz.com/torch/torch_nn-simple_layers-linear.html
+      s:add(nn.Linear(inputSize, dimensions)) -- layer 1
+   else
    
-   -- http://www.epcsirmaz.com/torch/torch_nn-transfer_function_layers-relu.html
-   s:add(nn.ReLU())
-   for i = 1, depth-1 do
-      s:add(nn.Linear(hiddenSize, hiddenSize))
-      s:add(nn.ReLU())
-   end   
-   s:add(nn.Linear(hiddenSize, dimensions))
+      s:add(nn.Linear(inputSize, hiddenSize)) -- layer 1
       
+      for i = 1, depth do
+	 -- http://www.epcsirmaz.com/torch/torch_nn-transfer_function_layers-relu.html
+	 s:add(nn.ReLU()) -- layer 2 ... depth*2
+	 if i < depth then
+	    s:add(nn.Linear(hiddenSize, hiddenSize)) -- layer 3 ...
+	 end
+      end   
+      s:add(nn.Linear(hiddenSize, dimensions)) -- layer depth*2+1
+   end
    
    -- The clustering head
    -- -------------------
@@ -43,19 +54,17 @@ local function createModel()
    -- in: (batchSize x dimensions) out: (batchSize x clusters)
    -- parameters: dimensions x clusters coordinates
    -- http://www.epcsirmaz.com/torch/torch_nn-simple_layers-euclidean.html -- https://github.com/torch/nn/blob/master/doc/simple.md#euclidean
-   s:add(nn.Euclidean(dimensions, clusters))
+   s:add(nn.Euclidean(dimensions, clusters)) -- layer depth*2+2
    
    -- in: (batchSize x clusters) out: (batchSize x clusters)
    -- parameters: clusters scalars
    -- we learn just a single factor per cluster, which is 1/radius
    -- http://www.epcsirmaz.com/torch/torch_nn-simple_layers-mul.html
-   s:add(nn.CMul(clusters))
+   s:add(nn.MulConstant(.01))
+   local r = nn.CMul(clusters)
+   r.weight:fill(1) -- TODO
+   s:add(r)
    
-   
-   -- http://www.epcsirmaz.com/torch/torch_nn-transfer_function_layers-tanh.html
-   -- TODO alternatively, use nn.Tanh
-   
-
    -- in: (batchSize x clusters) out: (batchSize x clusters)
    -- parameters: none
    -- http://www.epcsirmaz.com/torch/torch_nn-simple_layers-square.html
@@ -71,12 +80,13 @@ local function createModel()
    -- http://www.epcsirmaz.com/torch/torch_nn-simple_layers-exp.html
    s:add(nn.Exp())
    
-   return s
+   return s:type(dtype)
+
 end
 
 -- Create some sample data to test the network
 local function createSample()
-   local numPerCluster = 10;
+   local numPerCluster = 5;
    
    -- Start from points in the low-dimensional space
    local points = torch.Tensor(numPerCluster * clusters, dimensions)
@@ -86,7 +96,7 @@ local function createSample()
    --Points at integer coordinates and some random points nearby
    for j = 1, clusters do
       local from = (j-1)*numPerCluster + 1
-      points[from]:random(clusters)
+      points[from]:random(clusters * 2)
       labels[from][j] = 1
       
       for i = 2, numPerCluster do
@@ -109,12 +119,22 @@ local function createSample()
       features[i][5] = points[i][1] + torch.normal(0, 1)
    end
    
-   return features, labels
+   return features:type(dtype), labels:type(dtype), points:type(dtype)
+end
+
+local function prettyprint(msg, tensor)
+   print(msg)
+   for i = 1, tensor:size(1) do
+      for j = 1, tensor:size(2) do
+	 io.write(string.format("% .2e", tensor[i][j]))
+      end
+      io.write("\n")
+   end
 end
 
 local model = createModel()
-local criterion = nn.AbsCriterion()
-local x, y = createSample()
+local criterion = nn.AbsCriterion():type(dtype)
+local x, y, points = createSample()
 
 local function trainStep(x, y, learningRate)
    local pred = model:forward(x)
@@ -126,17 +146,24 @@ local function trainStep(x, y, learningRate)
    return err
 end
 
-trainStep(
-   torch.Tensor(inputSize),
-   torch.Tensor(clusters),
-   0.01
-)
-
-for i = 1, 100000 do
-   local err = trainStep(x, y, 0.01)
-   if i % 1000 == 0 then
-      print(err)
+local err
+while true do
+   for i = 1, 1000 do
+      err = trainStep(x, y, 0.01)
    end
+   print(err)
+   prettyprint("sample embedded", points:t())
+   prettyprint("modelled embedding", model:get(depth*2+1).output:t()) -- the last Linear layer outputting the embedded points
+   prettyprint("distances", model:get(depth*2+2).output:t()) -- the Euclidean
+   prettyprint("centres", model:get(depth*2+2).weight) -- the Euclidean
+   prettyprint("mult by radii", model:get(depth*2+3).output:t()) -- the Euclidean
+   print("radii")
+   print(model:get(depth*2+4).weight) -- CMul
+   prettyprint("square", model:get(depth*2+5).output:t())
+   prettyprint("exp", model.output:t()) -- the pairwise probabilities of belonging to the clusters
+   
+   prettyprint("y", y:t())
+   
 end
 
 -- print(mlp:get(1).weight)
