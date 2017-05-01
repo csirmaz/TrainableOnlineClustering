79ef87109324cf540516eda386f67e622be7ee3f
Elod Csirmaz <csirmazelodpal@gmail.com>
2017-05-01 15:43:48 +0100

    Finding the proper criterion for a test network

--- TrainCluster.lua
+++ TrainCluster.lua
@@ -1,5 +1,6 @@
 require 'torch'
 require 'nn'
+require 'optim'
 
 require 'cutorch'
 require 'cunn'
@@ -7,12 +8,37 @@ require 'cunn'
 -- cutorch.setDevice(1)
 dtype = 'torch.CudaTensor'
 
+local inputSize = 25; -- size of input
+local dimensions = 4; -- number of dimensions of the embedding space
+local clusters = 14; -- number of clusters / classes
+local hiddenSize = 32; -- size of hidden layers
+local depth = 3; -- number of hidden layers
+local OptimState = {learningRate = 0.01}
 
-local inputSize = 5;
-local dimensions = 2;
-local clusters = 4;
-local hiddenSize = 12;
-local depth = 2;
+local function prettyprint(msg, tensor)
+   print(msg)
+   for i = 1, tensor:size(1) do
+      for j = 1, tensor:size(2) do
+         io.write(string.format("% .3e", tensor[i][j]))
+      end
+      io.write("\n")
+   end
+end
+
+local function createTestModel()
+   local s = nn.Sequential()
+   s:add(nn.Linear(inputSize, hiddenSize))
+   for i = 1, depth do
+      s:add(nn.ReLU())
+      if i < depth then
+         s:add(nn.Linear(hiddenSize, hiddenSize)) -- layer 3 ...
+      end
+   end
+   s:add(nn.ReLU())
+   s:add(nn.Linear(hiddenSize, clusters))
+   s:add(nn.SoftMax())
+   return s:type(dtype)
+end
 
 local function createModel()
    
@@ -32,11 +58,11 @@ local function createModel()
       s:add(nn.Linear(inputSize, hiddenSize)) -- layer 1
       
       for i = 1, depth do
-	 -- http://www.epcsirmaz.com/torch/torch_nn-transfer_function_layers-relu.html
-	 s:add(nn.ReLU()) -- layer 2 ... depth*2
-	 if i < depth then
-	    s:add(nn.Linear(hiddenSize, hiddenSize)) -- layer 3 ...
-	 end
+         -- http://www.epcsirmaz.com/torch/torch_nn-transfer_function_layers-relu.html
+         s:add(nn.ReLU()) -- layer 2 ... depth*2
+         if i < depth then
+            s:add(nn.Linear(hiddenSize, hiddenSize)) -- layer 3 ...
+         end
       end   
       s:add(nn.Linear(hiddenSize, dimensions)) -- layer depth*2+1
    end
@@ -60,9 +86,11 @@ local function createModel()
    -- parameters: clusters scalars
    -- we learn just a single factor per cluster, which is 1/radius
    -- http://www.epcsirmaz.com/torch/torch_nn-simple_layers-mul.html
-   s:add(nn.MulConstant(.01))
+   
+   -- s:add(nn.MulConstant(.01))
+
    local r = nn.CMul(clusters)
-   r.weight:fill(1) -- TODO
+   -- r.weight:fill(1) -- TODO
    s:add(r)
    
    -- in: (batchSize x clusters) out: (batchSize x clusters)
@@ -81,91 +109,93 @@ local function createModel()
    s:add(nn.Exp())
    
    return s:type(dtype)
-
 end
 
 -- Create some sample data to test the network
+-- Returns:
+--   x - input to the model
+--   y -
+--   targetlabels
 local function createSample()
-   local numPerCluster = 5;
    
-   -- Start from points in the low-dimensional space
-   local points = torch.Tensor(numPerCluster * clusters, dimensions)
-   local labels = torch.Tensor(numPerCluster * clusters, clusters):zero()
-   local features = torch.Tensor(numPerCluster * clusters, inputSize)
-   
-   --Points at integer coordinates and some random points nearby
-   for j = 1, clusters do
-      local from = (j-1)*numPerCluster + 1
-      points[from]:random(clusters * 2)
-      labels[from][j] = 1
+   local data = require '../LearnPixels/output/LearnPixels.lua'
+   local datasize = #data
+   -- datasize = 15 -- TODO
+
+   local x = torch.Tensor(datasize, inputSize)
+   -- local y = torch.Tensor(datasize) -- target is class index
+   local y = torch.Tensor(datasize, clusters):zero()
+   local targetlabels = torch.Tensor(datasize, 1)
+
+   for i = 1, datasize do
+      local d = data[i]
       
-      for i = 2, numPerCluster do
-	 local to = (j-1)*numPerCluster + i
-	 -- print(from .. ":" .. to)
-	 labels[to][j] = 1
-	 for d = 1, dimensions do
-	    points[to][d] = torch.normal(points[from][d], 0.1)
-	 end
+      for j, pixel in ipairs(d.img) do
+         x[i][j] = pixel
       end
+      
+      -- d.classnum = (i > clusters/2 and clusters or i)-1 -- TODO
+      y[i][d.classnum+1] = 1
+      -- y[i] = d.classnum+1 -- target is class index
+      targetlabels[i][1] = d.classnum+1
    end
    
-   -- Translate the points to features
-   assert(inputSize == 5, "code supports inputSize = 5")
-   for i = 1, numPerCluster * clusters do
-      features[i][1] = points[i][1] + points[i][2]
-      features[i][2] = points[i][1] - points[i][2]
-      features[i][3] = 1 - points[i][1] + points[i][2] * 2
-      features[i][4] = 2 - points[i][1] / 2 - points[i][2]
-      features[i][5] = points[i][1] + torch.normal(0, 1)
-   end
+   print("x"); print(x)
+   print("y"); print(y)
+   return x:type(dtype), y:type(dtype), targetlabels:long()
+end
+
+
+local model = createTestModel()
+local criterion = nn.MSECriterion():type(dtype) -- nn.AbsCriterion():type(dtype)
+local x, y, targetlabels = createSample()
+local rep = 0
+
+local params, gradParams = model:getParameters()
+
+function debug(err)
+   rep = rep + 1
+   if rep < 10000 then return end
+   rep = 0
    
-   return features:type(dtype), labels:type(dtype), points:type(dtype)
+   local output = model.output
+   local _, predictions = output:float():sort(2, true) -- class indices ordered by probability
+   local correct = predictions:eq(
+      targetlabels:expandAs(output)
+   )
+   local correctratio = correct:narrow(2, 1, 1):sum() / output:size(1)
+
+   print(err, correctratio, OptimState.learningRate)
+   -- prettyprint("sample embedded", points:t())
+   -- prettyprint("modelled embedding", model:get(depth*2+1).output:t()) -- the last Linear layer outputting the embedded points
+   -- prettyprint("distances", model:get(depth*2+2).output:t()) -- the Euclidean
+   -- prettyprint("centres", model:get(depth*2+2).weight) -- the Euclidean
+   
+   -- prettyprint("mult by radii", model:get(depth*2+4).output:t())
+   -- print("radii")
+   -- print(model:get(depth*2+4).weight) -- CMul
+   
+   -- prettyprint("res", model.output)
+   -- prettyprint("exp", model.output:t()) -- the pairwise probabilities of belonging to the clusters
+   
+   -- prettyprint("y", y:t())
+   
 end
 
-local function prettyprint(msg, tensor)
-   print(msg)
-   for i = 1, tensor:size(1) do
-      for j = 1, tensor:size(2) do
-	 io.write(string.format("% .2e", tensor[i][j]))
-      end
-      io.write("\n")
-   end
-end
-
-local model = createModel()
-local criterion = nn.AbsCriterion():type(dtype)
-local x, y, points = createSample()
-
-local function trainStep(x, y, learningRate)
+function feval(params)
+   gradParams:zero()
    local pred = model:forward(x)
    local err = criterion:forward(pred, y)
    local gradCriterion = criterion:backward(pred, y)
-   model:zeroGradParameters()
    model:backward(x, gradCriterion)
-   model:updateParameters(learningRate)
-   return err
+   
+   debug(err)
+
+   return err, gradParams
 end
 
-local err
 while true do
-   for i = 1, 1000 do
-      err = trainStep(x, y, 0.01)
-   end
-   print(err)
-   prettyprint("sample embedded", points:t())
-   prettyprint("modelled embedding", model:get(depth*2+1).output:t()) -- the last Linear layer outputting the embedded points
-   prettyprint("distances", model:get(depth*2+2).output:t()) -- the Euclidean
-   prettyprint("centres", model:get(depth*2+2).weight) -- the Euclidean
-   prettyprint("mult by radii", model:get(depth*2+3).output:t()) -- the Euclidean
-   print("radii")
-   print(model:get(depth*2+4).weight) -- CMul
-   prettyprint("square", model:get(depth*2+5).output:t())
-   prettyprint("exp", model.output:t()) -- the pairwise probabilities of belonging to the clusters
-   
-   prettyprint("y", y:t())
-   
+   optim.adam(feval, params, OptimState)
 end
-
--- print(mlp:get(1).weight)
 
 
