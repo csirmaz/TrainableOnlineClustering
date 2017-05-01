b2fb329828688b1cdcc941b51573362d0f9ca3d0
Elod Csirmaz <csirmazelodpal@gmail.com>
2017-05-02 00:25:56 +0100

    Plotting test data

--- TrainCluster.lua
+++ TrainCluster.lua
@@ -1,6 +1,7 @@
 require 'torch'
 require 'nn'
 require 'optim'
+require 'gnuplot'
 
 require 'cutorch'
 require 'cunn'
@@ -8,11 +9,11 @@ require 'cunn'
 -- cutorch.setDevice(1)
 dtype = 'torch.CudaTensor'
 
-local inputSize = 25; -- size of input
-local dimensions = 4; -- number of dimensions of the embedding space
-local clusters = 14; -- number of clusters / classes
-local hiddenSize = 32; -- size of hidden layers
-local depth = 3; -- number of hidden layers
+local InputSize = 25; -- size of input
+local Dimensions = 4; -- number of dimensions of the embedding space
+local Clusters = 14; -- number of clusters / classes
+local HiddenSize = 32; -- size of hidden layers
+local Depth = 3; -- number of hidden layers
 local OptimState = {learningRate = 0.01} -- 0.01
 local layerIndex = {}
 
@@ -33,24 +34,24 @@ local function createModel()
    -- The embedding network
    -- ---------------------
 
-   -- inputSize == ?
-   -- outputSize == dimensions
+   -- InputSize == ?
+   -- outputSize == Dimensions
    
-   if depth == 0 then
+   if Depth == 0 then
       -- http://www.epcsirmaz.com/torch/torch_nn-simple_layers-linear.html
-      s:add(nn.Linear(inputSize, dimensions))
+      s:add(nn.Linear(InputSize, Dimensions))
    else
    
-      s:add(nn.Linear(inputSize, hiddenSize))
+      s:add(nn.Linear(InputSize, HiddenSize))
       
-      for i = 1, depth do
+      for i = 1, Depth do
          -- http://www.epcsirmaz.com/torch/torch_nn-transfer_function_layers-relu.html
-         s:add(nn.ReLU()) -- layer 2 ... depth*2
-         if i < depth then
-            s:add(nn.Linear(hiddenSize, hiddenSize))
+         s:add(nn.ReLU())
+         if i < Depth then
+            s:add(nn.Linear(HiddenSize, HiddenSize))
          end
       end   
-      s:add(nn.Linear(hiddenSize, dimensions))
+      s:add(nn.Linear(HiddenSize, Dimensions))
    end
    
    layerIndex["embedout"] = s:size()
@@ -58,17 +59,17 @@ local function createModel()
    -- The clustering head
    -- -------------------
    
-   -- inputSize == dimensions
-   -- outputSize == clusters
+   -- InputSize == Dimensions
+   -- outputSize == Clusters
 
    -- Here we implement clustering based on the following distance function:
    -- e^(-distance^2/radius^2)
    
    -- calculate distance to cluster centers
-   -- in: (batchSize x dimensions) out: (batchSize x clusters)
-   -- parameters: dimensions x clusters coordinates
+   -- in: (batchSize x Dimensions) out: (batchSize x Clusters)
+   -- parameters: Dimensions x Clusters coordinates
    -- http://www.epcsirmaz.com/torch/torch_nn-simple_layers-euclidean.html -- https://github.com/torch/nn/blob/master/doc/simple.md#euclidean
-   s:add(nn.Euclidean(dimensions, clusters)) -- layer depth*2+2
+   s:add(nn.Euclidean(Dimensions, Clusters))
    layerIndex["euclid"] = s:size()
    
    return s:type(dtype)
@@ -79,13 +80,13 @@ end
 --   x - input to the model
 --   y - -1/1 encoding
 --   targetlabels
-local function createSample()
+local function createSample(filename, create_y)
    
-   local data = require '../LearnPixels/output/LearnPixels.lua'
+   local data = require(filename)
    local datasize = #data
 
-   local x = torch.Tensor(datasize, inputSize)
-   local y = torch.Tensor(datasize, clusters):fill(-1)
+   local x = torch.Tensor(datasize, InputSize)
+   local y = torch.Tensor(datasize, Clusters):fill(-1)
    local targetlabels = torch.Tensor(datasize, 1)
 
    for i = 1, datasize do
@@ -95,38 +96,45 @@ local function createSample()
          x[i][j] = pixel
       end
       
-      y[i][d.classnum+1] = 1
+      if create_y then
+        y[i][d.classnum+1] = 1
+      end
       targetlabels[i][1] = d.classnum+1
    end
    
    return x:type(dtype), y:type(dtype), targetlabels:long()
 end
 
+-- --------------------
+-- TRAIN
+-- --------------------
 
 local model = createModel()
 
 -- https://github.com/torch/nn/blob/master/doc/criterion.md#hingeembeddingcriterion
 local criterion = nn.HingeEmbeddingCriterion(1.0):type(dtype)
 
-local x, y, targetlabels = createSample()
+local x, y, targetlabels = createSample('../LearnPixels/output/train.lua', true)
 
 local rep = 0
 
+local keep_training = true
+
 local params, gradParams = model:getParameters()
 
-function debug(err)
+function evaluate(err, prediction)
    rep = rep + 1
    if rep < 1000 then return end
    rep = 0
    
-   local output = model.output
-   local _, predictions = output:float():sort(2) -- class indices ordered by distance
+   -- prediction == model.output
+   local _, predictions = prediction:float():sort(2) -- class indices ordered by distance
    local correct = predictions:eq(
-      targetlabels:expandAs(output)
+      targetlabels:expandAs(prediction)
    )
-   local correctratio = correct:narrow(2, 1, 1):sum() / output:size(1)
+   local correctratio = correct:narrow(2, 1, 1):sum() / prediction:size(1)
 
-   -- prettyprint("modelled embedding", model:get(layerIndex["embedout"]).output:t())
+   -- prettyprint("modelled embedding", model:get(layerIndex["embedout"]).output)
 
    -- prettyprint("distances", model:get(layerIndex["euclid"]).output)
    -- prettyprint("centres", model:get(layerIndex["euclid"]).weight)
@@ -135,6 +143,8 @@ function debug(err)
    -- prettyprint("pred", predictions)
    
    print("Error", err, "Correct%", correctratio*100)
+   
+   if correctratio > 0.99 then keep_training = false end
 end
 
 function feval(params)
@@ -144,13 +154,57 @@ function feval(params)
    local gradCriterion = criterion:backward(pred, y)
    model:backward(x, gradCriterion)
    
-   debug(err)
+   evaluate(err, pred)
 
    return err, gradParams
 end
 
-while true do
+while keep_training do
    optim.adam(feval, params, OptimState)
 end
 
+local ClusterCenters = model:get(layerIndex["euclid"]).weight:float()
+
+-- --------------------
+-- TEST
+-- --------------------
+
+-- Run the model on the test set and see how it embeds the images
+-- in the low-dimensional space
+function test()
+   local input, _, testlabels = createSample('../LearnPixels/output/test.lua', false)
+   model:forward(input)
+   local embedding = model:get(layerIndex["embedout"]).output:float()
+   
+   local plotdata = {}
+
+   for i = 1, input:size(1) do
+      local label = testlabels[i][1]
+      local point = embedding[i]
+      
+      if not plotdata[label] then
+         plotdata[label] = {("Class %d"):format(label), {}, {}, {}}
+      end
+      
+      table.insert(plotdata[label][2], point[1]) -- x
+      table.insert(plotdata[label][3], point[2]) -- y
+      table.insert(plotdata[label][4], point[3]) -- z
+      
+   end
+   
+   local plotargs = {}
+   for _, v in pairs(plotdata) do
+      table.insert(plotargs, {
+         v[1],
+         torch.Tensor(v[2]),
+         torch.Tensor(v[3]),
+         torch.Tensor(v[4])
+      })
+   end
+   
+   gnuplot.scatter3(unpack(plotargs))
+
+end
+
+test()
 
